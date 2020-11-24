f7a042529a969b11fa44c879becc9816dc453986
croutonn <croutonn@users.noreply.github.com>
2020-11-24 18:10:32 +0000

    deploy: af922c72bcbe02fe71ea5dc87d4ce6017505bb1e

---
+++ index.87b4001d87673912ba82.js
@@ -0,0 +1,1 @@
+!function(){"use strict";var e=["example-1"];window.addEventListener("DOMContentLoaded",(function(){!function(n,t){void 0===t&&(t=e);var d=document.createElement("ul");t.forEach((function(e){var n=document.createElement("li"),t=document.createElement("a");t.innerText=e,t.href=e+".html",n.appendChild(t),d.appendChild(n)})),n.appendChild(d)}(document.body)}))}();
